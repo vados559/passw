e0f0a58b2e416180378e216db4a26f17e4117fe2
= <=>
2024-02-08 17:55:52 +0300

    Переход на словарь, сделана запись пароля в файл

--- Passsword.py
+++ Passsword.py
@@ -2,15 +2,23 @@ import inline # библиотека для изменения input
 def AdminAbil():
     while True:
         pass
-def UserAbil(nick, password):
-    choice = input("Функции:\n1.Сменить пароль\n>>")
+def UserAbil(nick, password, users, path):
+    choice = int(input("Функции:\n1.Сменить пароль\n>> "))
     if choice == 1:
         for i in range(3):
-            check = input2("Введите старый пароль: ")
-            if password == check:
-                pass # НАПИСАТЬ ЗАМЕНУ ПАРОЛЯ ПОЛЬЗОВАТЕЛЯ В ФАЙЛЕ
+            check = input2("Введите старый пароль: ", secret=True)
+            if password == check: # подтверждение пароля
+                password = input2("Введите новый пароль: ", secret=True)
+                users[nick] = password # обновление пароля в словаре
+                bd = open(path, 'w+')
+                for i in users: # обновление данных в файле
+                    bd.write(f"{i} {users[i]}\n")
+                print("Пароль успешно сменен!")
+                exit(1)         
             else:
                 print("Неверный пароль, попробуйте снова.\n")
+        print("Превышено число ошибочных вводов пароля.")
+        exit(0)
 
 
         
@@ -19,30 +27,30 @@ def UserAbil(nick, password):
 path = 'C:/Users/ilyxa/OneDrive/Desktop/passw/users.txt'
 bd = open(path, 'r')
 lines = bd.readlines()
-users = [ [0, 0] for _ in range(len(lines))] # матрица для хранения имен и паролей пользователей
+bd.close()
+users = {} # словарь для хранения имен и паролей пользователей
 
 for i in range(len(lines)): # цикл для подгрузки пользователей в программу
-    users[i][0], users[i][1] = lines[i].split() 
-    print(f"Имя: {users[i][0]}\t Пароль: {users[i][1]} \n")
+    username, password = lines[i].split()
+    users[username] = password 
+    print(f"Имя: {username}\t Пароль: {users[username]} \n")
 nick = input("Имя пользователя: ")
-if nick == users[0][0]:
+if nick == "admin": # проверка админ ли входит в систему
     for i in range(3):
         input2 = inline.input
         password = input2("Пароль:", secret = True) 
-        if password == users[0][1]:
+        if password == users[nick]:
             AdminAbil()
         else:
             print("Неверный пароль, попробуйте снова.")
-else:
-    for i in users:
-        if nick == i[0]:
-            for j in range(3):
-                input2 = inline.input
-                password = input2("Пароль:", secret = True)
-                if password == i[1]:
-                    UserAbil(nick, password)
-                else:
-                    print("Неверный пароль, попробуйте снова.\n")
+elif nick in users: # поиск пользователя в базе
+    for j in range(3):
+        input2 = inline.input # изменение input
+        password = input2("Пароль:", secret = True) # сокрытие введенных символов за *
+        if password == users[nick]: # проверка пароля
+            UserAbil(nick, password, users, path)
+        else:
+            print("Неверный пароль, попробуйте снова.\n")
 
 
 
